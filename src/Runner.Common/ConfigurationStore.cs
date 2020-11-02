10c68aa6e4a36650ff14992ce4ba82b23d160214
Ash Berlin-Taylor <ash_github@firemirror.com>
2020-11-02 15:19:50 +0000

    Add runner-level config to enforce PR security
    This allows self-hosted runners to limit who can run jobs to give "just
    enough protection" to allow using self-hosted runners with public repos.
    
    By default, the current behaviour is unchanged -- all jobs passed to the
    runner are executed.
    
    If the `.runner` config file has this block added to it:
    
    ```
      "pullRequestSecurity": {}
    ```
    
    Then by only PRs from "CONTRIBUTORS" and "OWNERS" can run (as defined by
    the field in
    https://docs.github.com/en/free-pro-team@latest/graphql/reference/objects#pullrequest
    -- nothing for us to have to work out ourselves.)
    
    It is also possible to explicitly list users that are allowed to run
    jobs on this worker:
    
    ```
      "pullRequestSecurity": {
        "allowedAuthors": ["ashb"]
      }
    ```
    
    Or to _only_ allow the given users, but not all contributors:
    
    ```
      "pullRequestSecurity": {
        "allowContributors": false,
        "allowedAuthors": ["ashb"]
      }
    ```
    
    Owners of the repo are always allowed to run jobs.
    
    If an allowed user pushes a commit to a PR from a non-allowed user, than
    _that_ build will be allowed to run on the self-hosted runner.

--- src/Runner.Common/ConfigurationStore.cs
+++ src/Runner.Common/ConfigurationStore.cs
@@ -1,5 +1,6 @@
 using GitHub.Runner.Sdk;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -49,6 +50,9 @@ namespace GitHub.Runner.Common
 
         [DataMember(EmitDefaultValue = false)]
         public string MonitorSocketAddress { get; set; }
+
+        [DataMember(Name = "PullRequestSecurity", EmitDefaultValue = false)]
+        public PullRequestSecuritySettings PullRequestSecuritySettings { get; set; }
 
         [IgnoreDataMember]
         public bool IsHostedServer
@@ -102,6 +106,18 @@ namespace GitHub.Runner.Common
                 _isHostedServer = null;
             }
         }
+    }
+
+    [DataContract]
+    public sealed class PullRequestSecuritySettings
+    {
+        // pullRequestSecurity is optional in the config -- if the key is
+        // defined, assume that we only want collaborators to run PRs.
+        [DataMember(EmitDefaultValue = false)]
+        public HashSet<string> AllowedAuthors = new HashSet<string>();
+
+        [DataMember(EmitDefaultValue = false)]
+        public bool AllowContributors = true;
     }
 
     [ServiceLocator(Default = typeof(ConfigurationStore))]
